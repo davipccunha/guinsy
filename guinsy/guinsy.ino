6d643745f4e71a6726b90af01e48ffe95f96d5e4
davipccunha <davi.pinheiro-costa-cunha@insa-lyon.fr>
2026-02-09 17:24:36 +0100

    feat: guitar input

--- guinsy/guinsy.ino
+++ guinsy/guinsy.ino
@@ -1,82 +1,99 @@
-//Librairies
-#include <Audio.h>
-//#include "MyDsp.h"
-#include "FaustGuitar.h"
+#include <Wire.h>
+#include "lib/NotesFrequencies/NoteFrequencies.h"
 
-//Bindings
-//Accessory nunchuck1;
-
-//MyDsp myDsp;
-FaustGuitar myDsp;
-AudioOutputI2S out;
-AudioControlSGTL5000 audioShield;
-AudioConnection patchCord0(myDsp,0,out,0);
-AudioConnection patchCord1(myDsp,1,out,1);
-
-
-#define DO 261.63
-#define RE 293.66
-#define MI 329.63
-#define SOL 392.0
-#define LA 440.0
+#define WII_ADDRESS 0x52
 
 void setup() {
-	AudioMemory(10);
-	Serial.begin(115200);
-	audioShield.enable(); // Active la puce SGTL5000
-  audioShield.volume(0.6); // Règle un volume de sortie
-}
+  Serial.begin(9600);
+  Wire.begin(); // On Teensy 4.0, this uses Pin 18 (SDA) and Pin 19 (SCL)
+  
+  // Handshake to disable encryption and initialize the controller
+  // This is the "modern" way to initialize Wii extensions
+  delay(100);
+  Wire.beginTransmission(WII_ADDRESS);
+  Wire.write(0xF0);
+  Wire.write(0x55);
+  Wire.endTransmission();
+  delay(10);
+  
+  Wire.beginTransmission(WII_ADDRESS);
+  Wire.write(0xFB);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  delay(20);
 
-void NotesToPlay(int G,int R, int Y, int B,int O){
-	if(G>10){
-		Play(DO);
-	}
-
-	if(R>10){
-		Play(RE);
-	}
-
-	if(Y>10){
-		Play(MI);
-	}
-
-	if(B>10){
-		Play(SOL);
-	}
-
-	if(O>10){
-		Play(LA);
-	}
-
-}
-
-void Play(int frequency){
-	myDsp.setParamValue("GUITARE_SOLO/GUITARE/frequence", frequency);
-	Settings();
-	//myDsp.setFreq(frequency);
-//	myDsp.setParamValue("/GUITARE_SOLO/GUITARE/pincer_la_corde", 1.0);
-delay(10);
-//	myDsp.setParamValue("/GUITARE_SOLO/GUITARE/pincer_la_corde", 0.0);
-}
-
-void Settings(){
-  //float frequency=analogRead(A3);
-  //int frequency = map(freq,0,1023,50,2000);
-	myDsp.setParamValue("GUITARE_SOLO/CONTROLES/echo", 0.3);
+  Serial.println("Wii Guitar Initialized");
 }
 
 void loop() {
-	/*
-	Serial.println("-------------------------------------------");
-	nunchuck1.readData();    // Read inputs and update maps
-	nunchuck1.printInputs(); // Print all inputs
-	for (int i = 0; i < WII_VALUES_ARRAY_SIZE; i++) {
-		Serial.println(
-				"Controller Val " + String(i) + " = "
-						+ String((uint8_t) nunchuck1.values[i]));
-	}
-	*/
-	//NotesToPlay(nunchuck1.values[11],nunchuck1.values[12],nunchuck1.values[13],nunchuck1.values[14],nunchuck1.values[15]);
-	NotesToPlay(25,0,0,0,0);
-	delay(2000);
+  // 1. Tell the guitar we want to read data by sending 0x00
+  Wire.beginTransmission(WII_ADDRESS);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  
+  delay(100); // Small delay for the guitar to prepare data
+
+  // 2. Request 6 bytes from the guitar
+  Wire.requestFrom(WII_ADDRESS, 6);
+  
+  uint8_t buffer[6];
+  int i = 0;
+  while (Wire.available() && i < 6) {
+    buffer[i] = Wire.read();
+    i++;
+  }
+
+  if (i == 6) {
+    parseGuitarData(buffer);
+  }
+
+  delay(100);
+}
+
+void parseGuitarData(uint8_t* data) {
+  // Byte 0: Joystick X (0-63)
+  // Byte 1: Joystick Y (0-63)
+  // Byte 2: Whammy Bar (0-31)
+  // Byte 3: Touch Bar (Slider on some guitars)
+  // Byte 4: Frets (Green, Red, Yellow, Blue, Orange)
+  // Byte 5: Buttons (Strum, Plus, Minus)
+
+  // Buttons are active LOW (0 means pressed, 1 means released)
+
+  uint8_t joyX = data[0] & 0x3F; // Range = 2-61
+  uint8_t joyY = data[1] & 0x3F; // Range = 2-61
+
+  bool green  = !(data[5] & 0x10);
+  bool red    = !(data[5] & 0x40);
+  bool yellow = !(data[5] & 0x08);
+  bool blue   = !(data[5] & 0x20);
+  bool orange = !(data[5] & 0x80);
+  
+  bool strumUp   = !(data[5] & 0x01);
+  bool strumDown = !(data[4] & 0x40);
+  
+  bool plus  = !(data[4] & 0x04);
+  bool minus = !(data[4] & 0x10);
+
+  int whammy = data[3] & 0x1F; // Range = 14-25
+
+  // Debug Output
+  if (green) Serial.print("G ");
+  if (red)   Serial.print("R ");
+  if (yellow)Serial.print("Y ");
+  if (blue)  Serial.print("B ");
+  if (orange)Serial.print("O ");
+  
+  if (strumUp)   Serial.print("STRUM_UP ");
+  if (strumDown) Serial.print("STRUM_DOWN ");
+
+  if (plus) Serial.print("+ ");
+  if (minus) Serial.print("- ");
+
+  Serial.printf("Whammy: %d\n", whammy);
+  Serial.printf("Joystick: (%d, %d)\n", joyX, joyY);
+  Serial.println();
+  
+  // Serial.print("Whammy: ");
+  // Serial.println(whammy);
 }
